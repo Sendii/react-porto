11addf09342df1189f8caaf604734ef0f6df9f64
Sendii <sendidianhadiwijaya@gmail.com>
2021-03-31 13:35:21 +0700

    centered star skill

--- src/component/template/Card.js
+++ src/component/template/Card.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import logo_github from '../../asset/github.png'
-import logo_fullstar from '../../asset/full-starr.png'
-import logo_halfstar from '../../asset/half-star.png'
-import logo_emptystar from '../../asset/empty-star.png'
+import logo_fullstar from '../../asset/full-star.png'
 
 class Card extends React.Component{
 	render(){
@@ -10,11 +8,13 @@ class Card extends React.Component{
 			<div className="card">
 			<img src={logo_github} alt="gambar" className="logo-skill"/>
 			<p>{this.props.name}</p>
-			<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
-			<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
-			<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
-			<img src={logo_halfstar} alt="gambar" className="rate-skill"/>
-			<img src={logo_emptystar} alt="gambar" className="rate-skill"/>
+			<div className="skill">
+				<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
+				<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
+				<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
+				<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
+				<img src={logo_fullstar} alt="gambar" className="rate-skill"/>
+			</div>
 			</div>
 			)
 	}
