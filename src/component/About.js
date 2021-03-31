522ef42c3c824100c4eab19e9637d8178c25b767
Sendii <sendidianhadiwijaya@gmail.com>
2021-03-31 16:03:51 +0700

    upd nav fixed top&position component about

--- src/component/About.js
+++ src/component/About.js
@@ -2,8 +2,38 @@ import Header from './template/Header'
 
 function About(){
 	return(
-		<div className="row">
+		<div className="row" id="about">
 			<Header keterangan="About" />
+			<div className="content">
+			<p>loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
+			<div className="content">
+			<p>ea1</p>
+			</div>
 		</div>
 		)
 }
