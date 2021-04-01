b2bea9b2c9974b1559d3b8f6b9cbd62e0a698c36
Sendii <sendidianhadiwijaya@gmail.com>
2021-04-01 14:24:08 +0700

    add fontawesome

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,5 +1,7 @@
 import Header from './template/Header'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function Contact(){
 	return(
 		<div className="row" id="contact">
@@ -7,14 +9,26 @@ function Contact(){
 			<div className="outer-card">
 				<div className="box">
 					<div className="in-box">
-						<img src="" alt="ea" />
-						<p>ini teks</p>
+						<FontAwesomeIcon icon={['fab', 'whatsapp']} />
+						<p>ini whatsapp</p>
 					</div>
 				</div>
 				<div className="box">
 					<div className="in-box">
-						<img src="" alt="ea" />
-						<p>ini teks</p>
+					<FontAwesomeIcon icon={['fab', 'linkedin']} />
+						<p>ini linkedin</p>
+					</div>
+				</div>
+				<div className="box">
+					<div className="in-box">
+						<FontAwesomeIcon icon={['fab', 'github']} />
+						<p>ini github</p>
+					</div>
+				</div>
+				<div className="box">
+					<div className="in-box">
+						<FontAwesomeIcon icon={['fab', 'google']} />
+						<p>ini gmail</p>
 					</div>
 				</div>
 			</div>
